893b285a7e06d7990653a857ff8656149690c4a4
Kumarteja2007 <kumarteja1506@gmail.com>
2026-08-30 22:49:12 +0530

    Prepare application for deployment

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ templates = Jinja2Templates(directory="templates")
 def get_db():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
